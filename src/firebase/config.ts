467c33227c27f47770f48e22b513aa0c6d7a1487
トヨタモビリティパーツ株式会社神奈川支社 <kanagawa.toyota.parts@gmail.com>
2025-10-21 06:17:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9545980025-bf83e",
+  "appId": "1:21224099607:web:9acfbae7cd9451e23af152",
+  "apiKey": "AIzaSyCl1WOEKb9hTh1cXh9TmmuKFzE0gR0hBxU",
+  "authDomain": "studio-9545980025-bf83e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "21224099607"
+};
