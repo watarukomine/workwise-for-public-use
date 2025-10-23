bb7f6c22007ab2abcc535d6295f7f6dcd1a28658
トヨタモビリティパーツ株式会社神奈川支社 <kanagawa.toyota.parts@gmail.com>
2025-10-23 08:49:26 +0000

    https://script.google.com/macros/s/AKfycbywC5IoTryeIFRyo7rU4hBaTb7u9p4aK

--- src/app/customers/page.tsx
+++ src/app/customers/page.tsx
@@ -7,7 +7,7 @@ import React from 'react';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { AlertCircle } from 'lucide-react';
 
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbw-QyW8LgFU1iiwipAhauNqukcd6hLxV8cDsdh0MmwIsgbP89pSsH58p680ZOB2etc8cA/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbywC5IoTryeIFRyo7rU4hBaTb7u9p4aK1p0UBvYeuzJiyVDaHqfjYeyA61seoH9LpeQYw/exec';
 
 export default function CustomersPage() {
   const [customers, setCustomers] = React.useState<any[]>([]);
