d63c808bdfde83b49e03f98c28cc5423de7776a3
トヨタモビリティパーツ株式会社神奈川支社 <kanagawa.toyota.parts@gmail.com>
2025-11-06 08:50:38 +0000

    https://script.google.com/macros/s/AKfycbyC-EcyhRN9kvnnLUNmdIflZIB2EpqIH

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -24,7 +24,7 @@ export const CUSTOMER_GAS_URL = 'https://script.google.com/macros/s/AKfycbygUg4b
  * 受注情報を取得・更新し、カレンダー連携も行うGoogle Apps ScriptのURL。
  * 主に order-context.tsx や schedule-view.tsx で使用されます。
  */
-export const ORDER_GAS_URL = 'https://script.google.com/macros/s/AKfycbx8oHickxKwp8jnZ69k4gslNH5v_bkC2Ksp-Y5EVtbe_p2VyKwy66LUZconraoXPBEU/exec';
+export const ORDER_GAS_URL = 'https://script.google.com/macros/s/AKfycbxC-EcyhRN9kvnnLUNmdIflZIB2EpqIHATqtlictAATiIIpqvTHOCVUDkDYoI0DXynM/exec';
 
 
 // --- スプレッドシート本体のURL ---
