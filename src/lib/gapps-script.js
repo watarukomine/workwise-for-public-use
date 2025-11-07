ef85483c258fb5f5ef17b84204ff8a5c6bf562f8
トヨタモビリティパーツ株式会社神奈川支社 <kanagawa.toyota.parts@gmail.com>
2025-11-07 03:36:47 +0000

    これは昨日の最後にやっていたエラーだと思いますが、スタッフマスタのスプレッドシートIDは以下の通りで 1Rd57UtLhhRigtOuMAkIf

--- src/lib/gapps-script.js
+++ src/lib/gapps-script.js
@@ -1,11 +1,12 @@
+
 // ↓↓↓↓【要設定】↓↓↓↓
 // 「受注管理」シートがあるスプレッドシートのIDを貼り付けてください
 const ORDER_SPREADSHEET_ID = "1Q3i81tz-j8GahLBRtdMJfnUjsx_VmM8fN7gn--j85JU"; 
 const ORDER_SHEET_NAME = "受注管理"; 
 
 // 「スタッフマスタ」シートがあるスプレッドシートのIDを貼り付けてください
-const STAFF_SPREADSHEET_ID = "1ojkHXVYFyomm-2RMbWq6QrG4NPCit2y6lxXQFsK_J60";
-const STAFF_SHEET_NAME = "Sheet1"; // 実際のシート名に合わせて変更してください
+const STAFF_SPREADSHEET_ID = "1Rd57UtLhhRigtOuMAkIfLnuQmuxwS9Mglr2_2feg4CE";
+const STAFF_SHEET_NAME = "スタッフマスタ"; // 実際のシート名に合わせて変更してください
 // ↓↓↓↓【設定はここまで】↓↓↓↓
 
 
