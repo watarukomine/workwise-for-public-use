276b10a2e2b5f5fb7669e2c931158aab1d0e5902
トヨタモビリティパーツ株式会社神奈川支社 <kanagawa.toyota.parts@gmail.com>
2025-11-11 06:12:03 +0000

    こんなエラーでます

--- src/lib/gapps-script.js
+++ src/lib/gapps-script.js
@@ -6,7 +6,7 @@ const ORDER_SHEET_NAME = "受注管理";
 
 // 「スタッフマスタ」シートがあるスプレッドシートのIDを貼り付けてください
 const STAFF_SPREADSHEET_ID = "1ojkHXVYFyomm-2RMbWq6QrG4NPCit2y6lxXQFsK_J60";
-const STAFF_SHEET_NAME = "スタッフマスタ"; // 実際のシート名に合わせて変更してください
+const STAFF_SHEET_NAME = "Sheet1"; // 実際のシート名に合わせて変更してください
 // ↓↓↓↓【設定はここまで】↓↓↓↓
 
 
