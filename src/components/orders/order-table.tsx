f5fff2f9f8a84dec2550944cf25e8fa8d37e9c03
トヨタモビリティパーツ株式会社神奈川支社 <kanagawa.toyota.parts@gmail.com>
2025-11-27 07:09:35 +0000

    情報が見えないのですが、行は表示されているので見えないだけですか？

--- src/components/orders/order-table.tsx
+++ src/components/orders/order-table.tsx
@@ -183,7 +183,7 @@ export function OrderTable({ orders: rawOrders, isLoading }: OrderTableProps) {
           <Table>
             <TableHeader>
               <TableRow>
-                {headers.map(header => <TableHead key={header} className="[writing-mode:vertical-rl] text-center h-40">
+                {headers.map(header => <TableHead key={header} className="[writing-mode:vertical-rl] text-center h-48">
                   {header}
                 </TableHead>)}
               </TableRow>
