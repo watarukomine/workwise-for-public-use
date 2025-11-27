36d67fbfc7e7c18bdec2e500c483d4858ec86046
トヨタモビリティパーツ株式会社神奈川支社 <kanagawa.toyota.parts@gmail.com>
2025-11-27 02:52:21 +0000

    {   "logName": "projects/workwisebu2-31559534-cd9ee/logs/run.googleapis.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        <link rel="manifest" href="/manifest.json" />
+        
       </head>
       <body className="font-body antialiased">
         <UserProfileProvider>
