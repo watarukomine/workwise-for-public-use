ea7b58e616e9e512cf2e00683c101c1716f9abb6
トヨタモビリティパーツ株式会社神奈川支社 <kanagawa.toyota.parts@gmail.com>
2025-11-28 05:17:11 +0000

    starting build "dc2fda4a-24c1-4e11-934b-52aa5ea689d4" FETCHSOURCE BUILD

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import * as React from 'react';
@@ -26,6 +27,7 @@ export default function DashboardPage() {
   const { 
     isLoading: isLoadingOrders, 
     statuses,
+    rawOrdersData,
   } = useOrder();
   
   const { isLoading: isLoadingCustomers, customers: allCustomers } = useCustomer();
