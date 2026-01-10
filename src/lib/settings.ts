0c905488cef098083515a57617b52d06100dec9b
マーケティング部IT推進室IT推進G トヨタ部品神奈川共販株式会社 <tmpmarketingsectionofkanagawa@TOYOTA-Mobility-Parts-Kanagawa-MacBook-Pro.local>
2026-01-10 15:45:47 +0900

    update: Set corrected GAS URL for order submission

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -24,7 +24,7 @@ export const STAFF_GAS_URL = 'https://script.google.com/macros/s/AKfycbxGeXICGCJ
 export const CUSTOMER_GAS_URL = 'https://script.google.com/macros/s/AKfycbz9CFA3PdNTi5fQhnxz5Dm6-copXjMPpCKvJtSGB80Na5cVSsdYXfgO-g5EQPK66BrcCA/exec';
 
 // Order & Task Management Script (Unified)
-export const ORDER_GAS_URL = 'https://script.google.com/macros/s/AKfycbzNAh55i7vsqSuBrIin5EEynpvjoY_GcvkiNAn6518cd--dWIgh8MhRiWWcDYJoDWUw/exec';
+export const ORDER_GAS_URL = 'https://script.google.com/macros/s/AKfycbyDqX6GZVeeFekZdyg7VDZnSj7T9YNN-pEskEcty-2IsN6s6vE5DzYlQgIGdCNXIvsY/exec';
 
 
 // --- スプレッドシート本体のURL ---
