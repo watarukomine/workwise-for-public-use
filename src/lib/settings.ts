d7ff6101a44a77175829188f079d4efb87a39b80
マーケティング部IT推進室IT推進G トヨタ部品神奈川共販株式会社 <tmpmarketingsectionofkanagawa@TOYOTA-Mobility-Parts-Kanagawa-MacBook-Pro.local>
2026-02-09 15:26:48 +0900

    chore: GAS WebアプリURLの更新 (最新デプロイ版の反映)

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -24,7 +24,7 @@ export const STAFF_GAS_URL = 'https://script.google.com/macros/s/AKfycbxGeXICGCJ
 export const CUSTOMER_GAS_URL = 'https://script.google.com/macros/s/AKfycbz9CFA3PdNTi5fQhnxz5Dm6-copXjMPpCKvJtSGB80Na5cVSsdYXfgO-g5EQPK66BrcCA/exec';
 
 // Order & Task Management Script (Unified)
-export const ORDER_GAS_URL = 'https://script.google.com/macros/s/AKfycbwSDhewV3Oj7GGR_PZk4xVsdS8AE5843LFgCjvLDkb59PXAoL_HQnziwouK923-sZxr/exec';
+export const ORDER_GAS_URL = 'https://script.google.com/macros/s/AKfycbykZbGkcJ3dwVLm56Af0wKVmGieJYTD-vlhoXjS8jntXhr2stTPT6ZtMJM0cVoqvUDf/exec';
 
 
 // --- スプレッドシート本体のURL ---
