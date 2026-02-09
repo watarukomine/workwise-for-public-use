b96b93e63e09980292123d47dc9fa5fbbebc156d
マーケティング部IT推進室IT推進G トヨタ部品神奈川共販株式会社 <tmpmarketingsectionofkanagawa@TOYOTA-Mobility-Parts-Kanagawa-MacBook-Pro.local>
2026-02-09 19:39:11 +0900

    chore: update ORDER_GAS_URL to the latest version

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -24,7 +24,7 @@ export const STAFF_GAS_URL = 'https://script.google.com/macros/s/AKfycbxGeXICGCJ
 export const CUSTOMER_GAS_URL = 'https://script.google.com/macros/s/AKfycbz9CFA3PdNTi5fQhnxz5Dm6-copXjMPpCKvJtSGB80Na5cVSsdYXfgO-g5EQPK66BrcCA/exec';
 
 // Order & Task Management Script (Unified)
-export const ORDER_GAS_URL = 'https://script.google.com/macros/s/AKfycby0cuJGToztJNJxYp0LKb37b21j_WIZBVoeok1NrCRpVd-1VISudcL6fzcf_kmu_fdN/exec';
+export const ORDER_GAS_URL = 'https://script.google.com/macros/s/AKfycbwedke_-9fhu-N4BXDo49vITXFa_ZnRZffi8IdoB-1poZage8x5Y8P-RxmkNnGSMuf1/exec';
 
 
 // --- スプレッドシート本体のURL ---
